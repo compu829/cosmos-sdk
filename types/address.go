377acb7daae8ae658ba5170f6239af30de8a8e80
Matjaž Čurič <matjaz.curic@comtrade.com>
2018-11-20 11:06:55 +0100

    BP-213 Change cosmos prefix to beyond in account and validator address

--- types/address.go
+++ types/address.go
@@ -18,17 +18,17 @@ const (
 	AddrLen = 20
 
 	// Bech32PrefixAccAddr defines the Bech32 prefix of an account's address
-	Bech32PrefixAccAddr = "cosmos"
+	Bech32PrefixAccAddr = "beyond"
 	// Bech32PrefixAccPub defines the Bech32 prefix of an account's public key
-	Bech32PrefixAccPub = "cosmospub"
+	Bech32PrefixAccPub = "beyondpub"
 	// Bech32PrefixValAddr defines the Bech32 prefix of a validator's operator address
-	Bech32PrefixValAddr = "cosmosvaloper"
+	Bech32PrefixValAddr = "beyondvaloper"
 	// Bech32PrefixValPub defines the Bech32 prefix of a validator's operator public key
-	Bech32PrefixValPub = "cosmosvaloperpub"
+	Bech32PrefixValPub = "beyondvaloperpub"
 	// Bech32PrefixConsAddr defines the Bech32 prefix of a consensus node address
-	Bech32PrefixConsAddr = "cosmosvalcons"
+	Bech32PrefixConsAddr = "beyondvalcons"
 	// Bech32PrefixConsPub defines the Bech32 prefix of a consensus node public key
-	Bech32PrefixConsPub = "cosmosvalconspub"
+	Bech32PrefixConsPub = "beyondvalconspub"
 )
 
 // ----------------------------------------------------------------------------
