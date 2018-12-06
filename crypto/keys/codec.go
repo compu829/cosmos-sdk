469383b7f2ada3ac57a98721fda3e515b3c772f3
Matjaž Čurič <matjaz.curic@comtrade.com>
2018-12-06 11:53:12 +0100

    Register PrivKeyDeepCoverSecp256r1 type into codec which holds a reference to DeepCover device

--- crypto/keys/codec.go
+++ crypto/keys/codec.go
@@ -13,7 +13,10 @@ func init() {
 	cdc.RegisterInterface((*Info)(nil), nil)
 	cdc.RegisterConcrete(ccrypto.PrivKeyLedgerSecp256k1{},
 		"tendermint/PrivKeyLedgerSecp256k1", nil)
+	cdc.RegisterConcrete(ccrypto.PrivKeyDeepCoverSecp256r1{},
+		"tendermint/PrivKeyDeepCoverSecp256r1", nil)
 	cdc.RegisterConcrete(localInfo{}, "crypto/keys/localInfo", nil)
 	cdc.RegisterConcrete(ledgerInfo{}, "crypto/keys/ledgerInfo", nil)
 	cdc.RegisterConcrete(offlineInfo{}, "crypto/keys/offlineInfo", nil)
+	cdc.RegisterConcrete(offlineInfo{}, "crypto/keys/deepCoverInfo", nil)
 }
